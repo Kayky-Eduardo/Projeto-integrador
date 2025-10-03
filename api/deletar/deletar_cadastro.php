182007bfb0f7fb9e55ce29ec156b98d81fa4d03f
Kayky-Eduardo <kayky002a@gmail.com>
2025-10-03 16:15:26 -0300

    Mudanças

--- api/deletar/deletar_cadastro.php
+++ api/deletar/deletar_cadastro.php
@@ -1,6 +1,6 @@
 <?php
 include "../../BD/conexao.php";
-Header('Content-Type: application/json');
+// Header('Content-Type: application/json');
 
 $id = isset($_GET['id']) ? intval($_GET['id']) : '';
 if (empty($id)) {
@@ -12,19 +12,18 @@ if (empty($id)) {
 $sqld = "DELETE FROM cargo WHERE id_cargo = ?";
 $stmt = $conn->prepare($sqld);
 $stmt->bind_param("i", $id);
-
+$stmt->execute()
 // Executa a consulta e verifica se foi bem-sucedida
-if ($stmt->execute()) {
-    if ($stmt->affected_rows > 0) {
-        echo json_encode(['success' => true, 'message' => 'Cadastro deletado com sucesso.']);
-    } else {
-        echo json_encode(['success' => false, 'message' => 'Nenhum cadastro encontrado com o ID fornecido.']);
-    }
-} else {
-    echo json_encode(['success' => false, 'message' => 'Erro ao deletar o cadastro: ' . $stmt->error]);
-}
+// if ($stmt->execute()) {
+//     if ($stmt->affected_rows > 0) {
+//         echo json_encode(['success' => true, 'message' => 'Cadastro deletado com sucesso.']);
+//     } else {
+//         echo json_encode(['success' => false, 'message' => 'Nenhum cadastro encontrado com o ID fornecido.']);
+//     }
+// } else {
+//     echo json_encode(['success' => false, 'message' => 'Erro ao deletar o cadastro: ' . $stmt->error]);
+// }
 
 // Fecha o statement e a conexão
-$stmt->close();
-$conn->close();
+// $stmt->close();
 ?>
--- public/index.php
+++ public/index.php
@@ -16,7 +16,7 @@ if(!isset($_SESSION)) {
         <h1>Sistema de RH</h1>
             <nav>
                 <a href="usuario/cadastro.php">cadastrar</a>
-                <a href="usuario/lista.php">Indefinido</a>
+                <a href="usuario/lista.php">listagem</a>
                 <a href="usuario/deletar.php"></a>
                 <a href="logout.php">Sair</a>
             </nav>
