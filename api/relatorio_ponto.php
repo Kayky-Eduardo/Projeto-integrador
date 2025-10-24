65dcc038d528ce6b3932026fa726d2dcd58e3b1c
Kayky-Eduardo <kayky002a@gmail.com>
2025-10-24 15:22:17 -0300

    ainda vou aprender o stash

--- api/relatorio_ponto.php
+++ api/relatorio_ponto.php
@@ -4,6 +4,9 @@ include(__DIR__ . "/../BD/conexao.php");
 $pesquisa_trabalhando = $conn->execute("
     SELECT * FROM ponto WHERE inicio_ponto IS NOT NULL AND (fim_ponto IS NULL OR data_fim = '');
 ");
-
+$numero_presente->get_result();
 $numero_presente = $pesquisa_trabalhando->num_rows;
+$pesquisa_pausa = $conn->excute("
+    SELECT * FROM ponto WHERE inicio_ponto IS NOT NULL AND (fim_ponto IS NULL OR data_fim = '');
+");
 ?>
