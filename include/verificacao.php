ba85f416452980416d157263b89fe214d84e6cb7
Kayky-Eduardo <kayky002a@gmail.com>
2025-10-24 06:52:35 -0300

    Chegando em um final

--- include/verificacao.php
+++ include/verificacao.php
@@ -1,16 +1,30 @@
 <?php
-    // função para verificar se o login do usuário é válido
-function verificar_login($conn) {
-    $stmt = $conn->prepare("
-        select data_fim from login where id_login = ?;     
-    ");
-    $stmt->bind_param("i", $_SESSION['id_login']);
-    $stmt->execute() or die("Falha ao executar o código SQL: " . $stmt->error);
-    $result = $stmt->get_result();
-    $encontrado = ($result->num_rows == 1) ? true : false;
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+// função para verificar se o login do usuário é válido
 
-    if(!isset($_SESSION['id_usuario']) || !$encontrado) {
-       return header("Location: logout.php");
+function verificar_login($conn) {
+    if (!isset($_SESSION['id_login']) || !isset($_SESSION['id_usuario'])) {
+        header("Location: logout.php");
+        exit;
+    }
+    $stmt = $conn->prepare("
+        SELECT data_fim FROM login
+        WHERE id_login = ? AND id_usuario = ? LIMIT 1
+    ");
+    $stmt->bind_param("ii", $_SESSION['id_login'], $_SESSION['id_usuario']);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    if ($result->num_rows === 0) {
+        header("Location: logout.php");
+        exit;
+    }
+
+    $row = $result->fetch_assoc();
+    if (!is_null($row['data_fim'])) {
+        header("Location: logout.php");
+        exit;
     }
 }
 ?>
