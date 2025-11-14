5a3a1502edf9fcd5c0e54d57eb177206878c8db0
Davi-Melo-Ferreira <davidemeloferreira13@gmail.com>
2025-11-13 22:22:19 -0300

    refatorando ponto

--- public/ponto/registrar_ponto.php
+++ public/ponto/registrar_ponto.php
@@ -16,7 +16,7 @@ $ponto = [];
     <h2>Registro de Ponto</h2>
 
     <form method="post" action="sql_registrar_ponto.php">
-        <a>Entrada</a>
+        <!-- <a>Entrada</a>
         <input type="time" name="entrada">
         <a>Saída</a>
         <input type="time" name="saida">
@@ -26,10 +26,50 @@ $ponto = [];
         <input type="time" name="almoco_saida">
         <a>Observações</a>
         <textarea name="observacao"></textarea>
-        <button type="submit" name="registro">Registrar</button>
-    </form>
+        <button type="submit" name="registro">Registrar</button> -->
 
-    <br>
+        <label for="entradas">Entrada:</label>
+
+        <select name="entradas" id="entrada">
+        <option value="entrada">Expediente</option>
+        <option value="almoco_entrada">Almoço</option>
+        </select>
+    </form>
+        <button id="btn">Iniciar</button>
+
+    <script>
+        const btn = document.getElementById("btn");
+        const select = document.getElementById("entrada");
+        let alternado = true;
+        var array = {
+            entrada: {
+                tempo_inicial:`${'tempo_atual'}`,
+                tempo_final:`${'tempo_atual'}`,
+                registrado: 0
+            },
+            almoco_entrada: {
+                tempo_inicial:`${'tempo_atual'}`,
+                tempo_final:`${'tempo_atual'}`,
+                registrado: 0
+            }
+        };
+
+        btn.addEventListener("click", () => {
+            alternado = !alternado;
+            btn.textContent = alternado ? "Iniciar" : "Sair";
+        });
+
+        select.addEventListener("change", () =>{
+            if (select.value = 'almoco_entrada'){
+                if (array.almoco_entrada.registrado < 2){
+                    btn.textContent = alternado ? "Iniciar" : "Sair";
+                }
+                
+            }
+            
+        })
+    </script>
+
     <?php
     if (empty($_POST['entrada']) || empty($_POST['saida'])) {
         echo "Preencha todos os campos obrigatórios.";
