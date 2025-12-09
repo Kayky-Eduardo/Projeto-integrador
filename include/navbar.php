ad08075ad5da4a28042d44f583a049e14629e432
Kayky-Eduardo <kayky002a@gmail.com>
2025-12-09 15:29:31 -0300

    atualização merge

--- include/navbar.php
+++ include/navbar.php
@@ -11,8 +11,8 @@ echo '<a href="/PROJETO-INTEGRADOR/public/index.php">Início</a>';
 if ($nivel >= 3) {
     echo '
         <a href="/PROJETO-INTEGRADOR/public/usuario/lista.php">Usuários</a>
-        <a href=#>Controle de Usuários</a>
-        <a href=#>Banco de Horas</a>
+        <a href="/Projeto-integrador/public/relatorio/online.php">Controle de usuarios</a>
+        <a href="/Projeto-integrador/public/meu_banco_horas.php">Banco de Horas</a>
         <a href=#>Indicadores</a>
         <a href="/PROJETO-INTEGRADOR/public/ponto/gerenciar.php">Gerenciar Pontos</a>
         <a href="/PROJETO-INTEGRADOR/public/rh/ajustes_pendentes.php">Ajustes Pendentes</a>
@@ -22,8 +22,8 @@ if ($nivel >= 3) {
 } elseif ($nivel == 2) {
     echo '
         <a href="/PROJETO-INTEGRADOR/public/usuario/lista.php">Usuários</a>
-        <a href=#>Controle de Usuários</a>
-        <a href=#>Banco de Horas</a>
+        <a href="/Projeto-integrador/public/relatorio/online.php">Controle de usuarios</a>
+        <a href="/Projeto-integrador/public/meu_banco_horas.php">Banco de Horas</a>
         <a href=#>Indicadores</a>
         <a href="/PROJETO-INTEGRADOR/public/ponto/status.php">Bater Ponto / Status</a>
         <a href="/PROJETO-INTEGRADOR/public/ponto/gerenciar.php">Gerenciar Pontos</a>
@@ -34,7 +34,7 @@ if ($nivel >= 3) {
 } else {
     echo '
         <a href="/PROJETO-INTEGRADOR/public/ponto/status.php">Bater Ponto / Status</a>
-        <a href=#>Banco de Horas</a>
+        <a href="/Projeto-integrador/public/meu_banco_horas.php">Banco de Horas</a>
         <a href="/PROJETO-INTEGRADOR/public/ponto/historico.php">Histórico</a>
         <a href="/PROJETO-INTEGRADOR/public/ponto/notificacoes.php">Notificações</a>
 ';
--- public/relatorio/online.php
+++ public/relatorio/online.php
@@ -2,16 +2,86 @@
 include(__DIR__ . "/../../BD/conexao.php");
 require "../../include/verificacao.php";
 verificar_login($conn);
-
-$verificacao_logado = $conn->prepare("
-SELECT id_login, email_login, data_inicio FROM login
-WHERE data_fim IS NULL OR data_fim = '';
-");
-$verificacao_logado->execute();
-$verificacao_logado = $verificacao_logado->get_result();
-$usuarios = [];
-echo "Usuarios logados: ";
-while ($row = $verificacao_logado->fetch_assoc()) {
-    echo "<p>id: " . $row['id_login'] . "<br>email: " . $row['email_login'] . "<br>inicio: " . $row['data_inicio'] . "</p>";
-}
+include "../../include/navbar.php";
 ?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Relatório ponto</title>
+    <link rel="stylesheet" href="../../assets/css/estilo.css">
+</head>
+<body>
+    <h1>Usuarios logados</h1>
+    <table>
+        <thead>
+            <th></th>
+            <th>ID usuario</th>
+            <th>Email</th>
+            <th>Entrada</th>
+            <th>Tempo logado</th>
+        </thead>
+        <tbody id="resposta-tbody">
+            <tr>
+                <td>-</td>
+                <td>-</td>
+                <td>-</td>
+                <td>-</td>
+            </tr>
+        </tbody>
+    </table>
+    <!-- Perfil de usuario geral(com filtro), porém irei fazer um para o profissional ver o próprio -->
+    <hr>
+</body>
+<script>
+    async function get_logados() {
+    // pegando a tabela
+    const resultado_relatorio = document.getElementById('resposta-tbody')
+    
+    // esperando a resposta em json da api
+    const resposta_api = await fetch(`../../api/api_relatorio_ponto.php?acao=get_logados`);
+    
+    // transformando a array em json para array normal
+    const resposta = await resposta_api.json();
+    
+    // esvaziando a tabela
+    resultado_relatorio.innerHTML = "";
+
+    // exibindo o resultado
+    resposta.forEach(r => {
+        let id = r.id_usuario ?? '-';
+        let email = r.email_login ?? '-';
+        let entrada = r.data_inicio ?? '-';
+        let tempo_logado = '-';
+        if (r.tempo_logado) {
+            const horas = Math.floor(r.tempo_logado / 60);
+            const minutos = r.tempo_logado % 60;
+            tempo_logado = horas > 0 ? `${horas}h ${minutos}min` : `${minutos}min`;
+        }
+        const tr = document.createElement("tr");
+        
+        tr.innerHTML = `
+            <td><button onclick="deslogar(${r.id_login})">Deslogar</button>
+            <td>${id}</td>
+            <td>${r.email_usuario}</td>
+            <td>${entrada}</td>
+            <td>${tempo_logado}</td>
+        `;
+        resultado_relatorio.appendChild(tr);
+        });
+    }
+    async function deslogar(id_login) {
+        const resposta = await fetch('../../api/api_relatorio_ponto.php?acao=deslogar', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ id_login: id_login })
+            });
+        get_logados();
+    }
+    get_logados();
+    setInterval(get_logados, 10000);
+</script>
+</html>
