f1521c8346a0cce966304c4fb8bef457b0163e88
Davi-Melo-Ferreira <davidemeloferreira13@gmail.com>
2026-02-10 13:16:33 -0300

    puxando arquivo de registrar ponto

--- public/ponto/pausas_e_ponto/registrar_ponto.php
+++ public/ponto/pausas_e_ponto/registrar_ponto.php
@@ -215,12 +215,12 @@ $tiposPausa = $stmtTipos->get_result();
                             <?php endwhile; ?>
                         </select>
 
-                        <?php if (!$pausaAtiva): ?>
+                        <?php if (!$pausaAtiva): $disabled = (!$pontoIniciado || $pontoFinalizado || $pausaAtiva) ? 'disabled' : '' ?>
                             <input type="hidden" name="acao" value="pausa_iniciar">
-                            <button type="submit" class="btn btn-padrao">Iniciar Pausa</button>
+                            <button type="submit" class="btn btn-padrao" <?= $disabled ?>>Iniciar Pausa</button>
                         <?php else: ?>
                             <input type="hidden" name="acao" value="pausa_finalizar">
-                            <button type="submit" class="btn btn-excluir" id="btnFinalizarPausa">Finalizar Pausa</button>
+                            <button type="submit" class="btn btn-excluir" id="btnFinalizarPausa" <?= $disabled ?>>Finalizar Pausa</button>
                             <p id="statusTempo" class="status-tempo"></p>
                         <?php endif; ?>
                     </form>
