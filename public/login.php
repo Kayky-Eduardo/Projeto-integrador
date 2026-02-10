d5e601e111ba751aa1204c4f0d63c7fde9c533a3
Kayky-Eduardo <kayky002a@gmail.com>
2026-02-10 13:08:30 -0300

    atualizando login

--- public/login.php
+++ public/login.php
@@ -127,7 +127,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     }
 
 ?>
-
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -163,4 +162,5 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         </section>
     </main>
 </body>
+
 </html>
